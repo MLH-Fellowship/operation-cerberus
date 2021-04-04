746a63027d3531943105a39930ef3056ff1b646c
dogurasu <dugs134@gmail.com>
2021-04-04 13:54:07 -0700

    Added file actions, reducer, types

--- client/src/redux/reducers/main.js
+++ client/src/redux/reducers/main.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import openDrawerReducer from './openDrawerReducer';
 import storeUserInfoReducer from './storeUserInfoReducer';
+import { pinChartReducer } from './chartReducers';
 
 const rootReducer = combineReducers({
   storeUserInfoReducer,
   openDrawerReducer,
+  chart: pinChartReducer,
 });
 
 export default rootReducer;
